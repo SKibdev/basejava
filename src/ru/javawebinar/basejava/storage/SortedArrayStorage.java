c7de56d0f2160ff461d1f76bc9734bccb84e5224
Sergei Kibardin <kibardinsa@gmail.com>
2024-01-17 19:18:08 +0300

    HW4 Changed comment in insertElement

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected void insertElement(Resume r, int index) {
-        // size уже увеличен в родительском классе
+        // size еще не увеличен в родительском классе
         index = -(index + 1);
         System.arraycopy(storage, index, storage, index + 1, size - index);
         storage[index] = r;
