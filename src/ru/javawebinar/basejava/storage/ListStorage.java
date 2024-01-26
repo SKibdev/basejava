f64184cc7466e6fee49874d49673ca17fd827c48
Sergei Kibardin <kibardinsa@gmail.com>
2024-01-26 22:57:58 +0300

    HW5 implemented ListStorage and created ListStorageTest

---
+++ src/ru/javawebinar/basejava/storage/ListStorage.java
@@ -0,0 +1,56 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.ArrayList;
+
+public class ListStorage extends AbstractStorage {
+    protected final ArrayList<Resume> storage = new ArrayList<>();
+
+    @Override
+    public final Resume[] getAll() {
+        return storage.toArray(storage.toArray(new Resume[0]));
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    public final void deleteElement(int index) {
+        storage.remove(index);
+        storage.trimToSize();
+    }
+
+    @Override
+    public final Resume getElement(int index) {
+        return storage.get(index);
+    }
+
+    @Override
+    public final void clear() {
+        storage.clear();
+    }
+
+    @Override
+    protected void replaceElement(int index, Resume r) {
+        storage.set(index, r);
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume(uuid);
+        return storage.indexOf(searchKey);
+    }
+
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage.add(r);
+    }
+//
+//    protected abstract void insertElement(Resume r, int index);
+//
+//    protected abstract void fillDeletedElement(int index);
+}
+
