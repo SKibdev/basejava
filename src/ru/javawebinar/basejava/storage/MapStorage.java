02079fe2da3368cf0a75aa38639881db94168319
Sergei Kibardin <kibardinsa@gmail.com>
2024-01-31 21:52:15 +0300

    HW5 implemented MapStorage and MapStorageTest. HW5 is ready.

--- src/ru/javawebinar/basejava/storage/MapStorage.java
+++ src/ru/javawebinar/basejava/storage/MapStorage.java
@@ -1,92 +1,60 @@
 package ru.javawebinar.basejava.storage;
 
-import ru.javawebinar.basejava.exception.ExistStorageException;
-import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-// implements Storage - для тестирования, временно
-public class MapStorage implements Storage/*extends AbstractStorage<String> */{
-    protected final Map<String, Resume> storage = new HashMap<>();
-// ТО ДО сделать полностью мап отдельно - готово
+
 // сравнить с Абстраксторожом и наконец вынести лишнее от массивов в абстрактаррайсторож
+public class MapStorage extends AbstractStorage<String> {
+    protected final Map<String, Resume> storage = new HashMap<>();
 
-    public final void doSave(Resume r) {
-        String uuid = r.getUuid();
-        String searchKey = getKey(uuid);
-        if (!searchKey.equals("-1")) {
-            throw new ExistStorageException(uuid);
-        }
-//        increaseSize(uuid);
-        insertElement(uuid, r);
-    }
-
-    public final void doUpdate(Resume r) {
-        String searchKey = getExistingSearchKey(r.getUuid());
-        replaceElement(searchKey, r);
-    }
-
-    public final void doDelete(String uuid) {
-        String searchKey = getExistingSearchKey(uuid);
-        deleteElement(searchKey);
-    }
-
-    public final Resume doGet(String uuid) {
-        String searchKey = getExistingSearchKey(uuid);
-        return getElement(searchKey);
-    }
-
-    protected final String getExistingSearchKey(String uuid) {
-        String searchKey = getKey(uuid);
-        if (searchKey.equals("-1")) {
-            throw new NotExistStorageException(uuid);
-        }
-        return searchKey;
-    }
-
-//    @Override
+    @Override
     public Resume[] doGetAll() {
         Collection<Resume> resumes = storage.values();
         return resumes.toArray(new Resume[0]);
     }
 
-//    @Override
+    @Override
     public int doGetSize() {
         return storage.size();
     }
 
-//    @Override
+    @Override
     public void doClear() {
         storage.clear();
     }
 
-//    @Override
-    protected String getKey(String uuid) {
-        if (storage.containsKey(uuid)) {
-            return uuid;
-        }
-        return "-1";
+    @Override
+    protected String getSearchKey(String uuid) {
+        return uuid;
     }
 
-//    @Override
-    protected void insertElement(String key, Resume r) {
-        storage.put(key, r);
+    @Override
+    protected boolean isExist(String searchKey) {
+        return storage.containsKey(searchKey);
     }
 
-//    @Override
-    protected void replaceElement(String key, Resume r) {
-        storage.put(key, r);
+    @Override
+    protected void insertElement(String searchKey, Resume r) {
+        storage.put(searchKey, r);
     }
 
-//    @Override
-    protected void deleteElement(String index) {
-        storage.remove(index);
+    @Override
+    protected void replaceElement(String searchKey, Resume r) {
+        storage.put(searchKey, r);
     }
 
-//    @Override
-    protected Resume getElement(String key) {
-        return storage.get(key);
+    @Override
+    protected void deleteElement(String searchKey) {
+        storage.remove(searchKey);
+    }
+
+    @Override
+    protected Resume getElement(String searchKey) {
+        return storage.get(searchKey);
     }
 }
+
+
