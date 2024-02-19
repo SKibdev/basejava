8eded40d653c29fc686025d2fc6787ff15164adb
Sergei Kibardin <kibardinsa@gmail.com>
2024-02-19 13:28:05 +0300

    HW 7 implemented ContactType

---
+++ src/ru/javawebinar/basejava/model/ContactType.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.basejava.model;
+
+public enum ContactType {
+
+    TELEPHONE("Тел."),
+    SKYPE("Skype"),
+    EMAIL("Почта"),
+    LINKEDIN("Профиль LinkedIn"),
+    GITHUB("Профиль GitHub"),
+    STACKOVERFLOW("Профиль Stackoverflow"),
+    HOMEPAGE("Домашняя страница"),
+    ;
+
+    private final String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
