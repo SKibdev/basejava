aa602b8e2fe94ec6f6af612f1e205aa76fb2c3b6
Sergei Kibardin <kibardinsa@gmail.com>
2024-03-13 13:33:21 +0500

    HW09 "printDirectoryDeeply" is implemented in "MainFilesRecursion".

--- src/ru/javawebinar/basejava/MainFilesRecursion.java
+++ src/ru/javawebinar/basejava/MainFilesRecursion.java
@@ -1,38 +1,31 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.util.Objects;
 
 public class MainFilesRecursion {
+    static final StringBuilder INDENTATION = new StringBuilder();
+    static final String INDENTATION_STEP = "    ";
+    static final int INDENTATION_STEP_LENGTH = INDENTATION_STEP.length();
+
     public static void main(String[] args) {
         File dir = new File(".\\");
-        printDirectoryDeeply1(dir);
-        printDirectoryDeeply2(dir);
+        printDirectoryDeeply(dir);
     }
 
-    static void printDirectoryDeeply1(File dir) {
-        if (dir.isDirectory()) {
-            File[] files = dir.listFiles();
-            for (File file : Objects.requireNonNull(files)) {
-                System.out.println(file.getName());
-                if (file.isDirectory()) {
-                    printDirectoryDeeply1(file);
-                }
-            }
-        }
-    }
-
-    // Вариант из разбора ДЗ. Отличается от моего, тем что выводит имя,
-    // даже если в метод подасться файл, а не директория. Есть  "File: " и "Directory: "
-    static void printDirectoryDeeply2(File dir) {
+    static void printDirectoryDeeply(File dir) {
         File[] files = dir.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(INDENTATION + "File: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    printDirectoryDeeply2(file);
+                    System.out.println(INDENTATION + "Directory: " + file.getName());
+                    INDENTATION.append(INDENTATION_STEP);
+                    printDirectoryDeeply(file);
+                    int indentationLength = INDENTATION.length();
+                    if (indentationLength >= INDENTATION_STEP_LENGTH) {
+                        INDENTATION.delete(indentationLength - INDENTATION_STEP_LENGTH, indentationLength);
+                    }
                 }
             }
         }
