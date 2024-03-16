d847df7f4f3ad2c3400ea100354f34b5d3f4885a
Sergei Kibardin <kibardinsa@gmail.com>
2024-03-16 18:12:01 +0500

    HW09 Added ObjectStreamStorageTest.class, ObjectStreamPathStorageTest.class

--- test/ru/javawebinar/basejava/storage/AllStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AllStorageTest.java
@@ -6,8 +6,11 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @SelectClasses({ArrayStorageTest.class,
         SortedArrayStorageTest.class,
+        ArrayStorageTest.class,
         ListStorageTest.class,
         MapUuidStorageTest.class,
-        MapResumeStorageTest.class})
+        MapResumeStorageTest.class,
+        ObjectStreamStorageTest.class,
+        ObjectStreamPathStorageTest.class})
 public class AllStorageTest {
 }
