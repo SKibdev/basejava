0884293c08d2cb3c5ee5e531c917d8987628cec7
Sergei Kibardin <kibardinsa@gmail.com>
2024-05-08 14:40:01 +0500

    HW16 @WebServlet, fixed button "Cancel"

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -7,11 +7,13 @@ import ru.javawebinar.basejava.storage.Storage;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name ="resumeServlet", urlPatterns = "/resume")
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
 
